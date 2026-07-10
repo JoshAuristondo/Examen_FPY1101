f7e779e62e63a24960b0e6b940008993dd0c89e7
PAAWSA03LC0523 <PAAWSA03LC0523@PAAWSA03LC0523.duoc.acad>
2026-07-10 09:32:31 -0400

    LOL

--- Joshua_Auristondo_B/Joshua_Auristondo_B.py
+++ Joshua_Auristondo_B/Joshua_Auristondo_B.py
@@ -12,7 +12,7 @@ def mostrar_menu():
 
 def leer_opcion():
     try:
-        opcion = int(input("\nIngrese opcion:\n-> "))
+        opcion = int(input("\nIngrese opcion: "))
         if 1 <= opcion <= 6:
             return opcion
         else:
@@ -50,8 +50,8 @@ def validar_clasificacion(clasificacion):
 def validar_idioma(idioma):
     return len(idioma) != 0
 
-def validar_3d(valor):
-    return valor.lower() in ["s", "n"] 
+def validar_3d(es_3d):
+    return es_3d.lower() in ["s", "n"] 
 
 def validar_precio(precio):
     try:
@@ -68,3 +68,116 @@ def validar_cupos(cupos):
         return False 
     
 ##Funciones principales
+
+def cupos_por_genero(peliculas, cartelera, genero):
+    total = 0
+
+    for codigo in peliculas:
+        if peliculas[codigo][1].upper() == genero.upper():
+            total += cartelera[codigo][1]
+    
+    if total != 0:
+        print(f"El total de cupos disponibles es: {total}")
+        return
+
+    print(f"No hay cupos para [{genero}]")
+
+def buscar_por_rango(peliculas, cartelera, p_min, p_max):
+    lista = []
+    for codigo in cartelera:
+        precio = cartelera[codigo][0]
+        cupos = cartelera[codigo][1]
+        if precio <= p_max and precio >= p_min and cupos != 0:
+            nombre = peliculas[codigo][0]
+            lista.append(nombre + "--" + codigo)
+
+    lista.sort()
+
+    if len(lista) != 0:
+        print(f"\nResultados de busqueda entre [{p_min} - {p_max}]:")
+        for pelicula in lista:
+            print(pelicula)
+    else:
+        print("No hay películas en ese rango de precios.")
+
+def buscar_codigo(peliculas, cartelera, codigo):
+    for codigo in peliculas:
+        return peliculas[codigo].upper() == codigo.upper()        
+
+def actualizar_precio(peliculas, cartelera, codigo, precio):
+    if not buscar_codigo(peliculas, cartelera, codigo):
+        return False
+    
+    if not validar_precio(precio):
+        return False
+    
+    cartelera[codigo][0] = precio
+    return True
+    
+def agregar_pelicula(peliculas, cartelera, codigo, titulo, genero, duracion, clasificacion, idioma, es_3d, precio, cupos):
+
+    if buscar_codigo(peliculas, cartelera, codigo):
+        return False
+
+    peliculas[codigo] = [
+        codigo,
+        titulo,
+        genero,
+        duracion,
+        clasificacion.upper(),
+        idioma,
+        es_3d.lower() == "s"
+    ]
+
+    cartelera[codigo] = [
+        precio,
+        cupos
+    ]
+
+    return True
+
+def eliminar_pelicula(peliculas, cartelera, codigo):
+    if not buscar_codigo(peliculas, cartelera, codigo):
+        return False
+
+    del peliculas[codigo]
+    del cartelera[codigo]
+    return True
+    
+def main():
+    peliculas = {}
+    cartelera = {}
+
+    opcion = 0
+
+    while opcion != 6:
+        mostrar_menu()
+        opcion = leer_opcion()
+        match opcion:
+            case 1:
+                if len(peliculas) == 0:
+                    print("\nNo hay peliculas\n")
+                else:
+                    genero = input("Ingrese género a consultar: ")
+                    cupos_por_genero(peliculas, cartelera, genero) 
+            case 2:
+                if len(peliculas) == 0:
+                    print("\nNo hay peliculas\n")
+                else:
+                    pass
+            case 3:
+                if len(peliculas) == 0:
+                    print("\nNo hay peliculas\n")
+                else:
+                    pass
+            case 4:
+                pass
+            case 5:
+                if len(peliculas) == 0:
+                    print("\nNo hay peliculas\n")
+                else:
+                    pass
+            case 6:
+                print("Programa finalizado.")
+
+main()
